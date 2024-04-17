52f4309c35df5d6d9b3d2412d2b49ceb81933c5b
Christopher Johnson <critterjohnson45@gmail.com>
2024-04-17 15:54:38 -0400

    better column names

--- pcap_dataset.py
+++ pcap_dataset.py
@@ -16,25 +16,25 @@ def get_pcap_dataframe(path):
     Converts each line of the pcap dataset csv into a 10-packet window.
     '''
     df = pandas.read_csv(path, dtype=str)
-    df = df.rename(columns=lambda x: x[1:].replace(' ', '_'))
+    df = df.rename(columns=lambda x: x.replace(' ', '_') if not x.startswith(' ') else x[1:].replace(' ', '_'))
 
-    df = df.drop(['nnamed:_0', 'low_ID',
+    df = df.drop(['Unnamed:_0', 'Flow_ID',
        'Flow_IAT_Mean', 'Flow_IAT_Std', 'Flow_IAT_Max', 'Flow_IAT_Min',
-       'wd_IAT_Total', 'Fwd_IAT_Mean', 'Fwd_IAT_Std', 'Fwd_IAT_Max',
-       'Fwd_IAT_Min', 'wd_IAT_Total', 'Bwd_IAT_Mean', 'Bwd_IAT_Std',
-       'Bwd_IAT_Max', 'Bwd_IAT_Min', 'wd_PSH_Flags', 'Bwd_PSH_Flags',
+       'Fwd_IAT_Total', 'Fwd_IAT_Mean', 'Fwd_IAT_Std', 'Fwd_IAT_Max',
+       'Fwd_IAT_Min', 'Fwd_IAT_Total', 'Bwd_IAT_Mean', 'Bwd_IAT_Std',
+       'Bwd_IAT_Max', 'Bwd_IAT_Min', 'Fwd_PSH_Flags', 'Bwd_PSH_Flags',
        'Fwd_URG_Flags', 'Bwd_URG_Flags', 'Fwd_Header_Length',
-       'Bwd_Header_Length', 'wd_Packets/s', 'Bwd_Packets/s',
+       'Bwd_Header_Length', 'Fwd_Packets/s', 'Bwd_Packets/s',
        'Min_Packet_Length', 'Max_Packet_Length', 'Packet_Length_Mean',
        'Packet_Length_Std', 'Packet_Length_Variance', 'Down/Up_Ratio',
        'Average_Packet_Size', 'Avg_Fwd_Segment_Size', 'Avg_Bwd_Segment_Size',
-       'Fwd_Header_Length.1', 'wd_Avg_Bytes/Bulk', 'Fwd_Avg_Packets/Bulk',
+       'Fwd_Header_Length.1', 'Fwd_Avg_Bytes/Bulk', 'Fwd_Avg_Packets/Bulk',
        'Fwd_Avg_Bulk_Rate', 'Bwd_Avg_Bytes/Bulk', 'Bwd_Avg_Packets/Bulk',
-       'wd_Avg_Bulk_Rate', 'ubflow_Fwd_Packets', 'Subflow_Fwd_Bytes',
-       'Subflow_Bwd_Packets', 'Subflow_Bwd_Bytes', 'nit_Win_bytes_forward',
+       'Fwd_Avg_Bulk_Rate', 'Subflow_Fwd_Packets', 'Subflow_Fwd_Bytes',
+       'Subflow_Bwd_Packets', 'Subflow_Bwd_Bytes', 'Init_Win_bytes_forward',
        'Init_Win_bytes_backward', 'act_data_pkt_fwd', 'min_seg_size_forward',
-       'ctive_Mean', 'Active_Std', 'Active_Max', 'Active_Min', 'dle_Mean',
-       'Idle_Std', 'Idle_Max', 'Idle_Min', 'imillarHTTP', 'Inbound'], axis=1)
+       'Active_Mean', 'Active_Std', 'Active_Max', 'Active_Min', 'Idle_Mean',
+       'Idle_Std', 'Idle_Max', 'Idle_Min', 'SimillarHTTP', 'Inbound'], axis=1)
 
     prompt_labels = []
     df["text"] = df.apply(combine(prompt_labels), axis=1)
